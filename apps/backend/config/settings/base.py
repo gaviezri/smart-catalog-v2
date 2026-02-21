679a6be3bbb2814adf20c9c814315fb655a5da9f
galaviezri <g.aviezri@gmail.com>
2026-02-21 17:21:44 +0200

    fix docker deployment

--- apps/backend/config/settings/base.py
+++ apps/backend/config/settings/base.py
@@ -77,6 +77,9 @@ REST_FRAMEWORK: dict = {
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
     ),
+    "DEFAULT_RENDERER_CLASSES": (
+        "rest_framework.renderers.JSONRenderer",
+    ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 20,
     "EXCEPTION_HANDLER": "config.exceptions.global_exception_handler",
