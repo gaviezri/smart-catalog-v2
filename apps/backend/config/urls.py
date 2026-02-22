67817883d5f1fa07d38f8ab6cc6e0046f445227f
galaviezri <g.aviezri@gmail.com>
2026-02-22 17:20:06 +0200

    fix broken urls

--- apps/backend/config/urls.py
+++ apps/backend/config/urls.py
@@ -6,12 +6,12 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 urlpatterns: list = [
-    path("api", include([
+    path("/api", include([
         # API
-        path("auth", include("users.application.urls")),
-        path("products", include("products.application.urls")),
+        path("/auth", include("users.application.urls")),
+        path("/products", include("products.application.urls")),
         # OpenAPI
-        path("openapi/schema", SpectacularAPIView.as_view(), name="schema"),
-        path("openapi/docs", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+        path("/openapi/schema", SpectacularAPIView.as_view(), name="schema"),
+        path("/openapi/docs", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     ])),
 ]
